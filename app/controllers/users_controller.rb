011f6251baa4ef15cbd904fcc572e89991813b65
takakotmt <st.0614.tmt@gmail.com>
2019-01-18 14:55:10 +0900

    二回目

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,86 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user,{only: [:edit,:update]}
+  before_action :forbid_login_user,{only: [:new,:create,:login_from,:login]}
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_name: "defalut_user.jpg",
+      password: params[:password]
+    )
+    @user.save
+    redirect_to("/users/index")
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}",image.read)
+    end
+    if @user.save
+      flash[:notice] = "ユーザー情報を編集しました"
+      redirect_to("/users/index")
+    else
+      render("/users/edit")
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    flash[:notice] = "削除しました"
+    redirect_to("/users/index")
+  end
+
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(
+      email: params[:email],
+      password: params[:password])
+      if @user
+        session[:user_id] = @user.id
+        flash[:notice]="ログインしました"
+        redirect_to("/posts/index")
+      else
+        @email = params[:email]
+        @password = params[:password]
+        @error_message = "メールアドレスまたはパスワードが間違っています"
+        render("/users/login_from")
+      end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+  end
+end
